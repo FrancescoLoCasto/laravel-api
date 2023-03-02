55c62f371b42d9ad22546c7033f2744b3d29efd9
MyUsername <my@email.com>
2023-03-02 16:41:18 +0100

    problem on post controller api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index() {
+        $posts = Post::all();
+
+        return response()->json([
+            'succes' => true,
+            'results' => $posts,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/posts',[PostController::class. 'index']);
+Route::get('/posts',[PostController::class. 'index'])->name('api.posts.index');
